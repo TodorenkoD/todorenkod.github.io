7cf83909f7df5273d323d4463bc6eb220ba1b91a
Todorenko Dmitriy <todorenko.d@gmail.com>
2017-05-13 14:50:23 +0300

    download homework 01-02

---
+++ JS/Homework/01-02/js/names.js
@@ -0,0 +1,23 @@
+'use scrict';
+
+var arr = [];
+
+	for (var i = 0; i < 5; i++) {
+	    arr[i] = prompt('Введите любое имя');
+	}
+
+	console.log(arr);
+
+var userName = prompt('Введите имя пользователя:');
+	console.log('Вас зовут ' + userName);
+
+for (var i = 0; i < 5; i++) {
+	if (arr[i] == userName) {
+	alert(userName + ', Вы успешно вошли');
+	break;
+	}
+}
+
+if ( i>=5 ){
+	alert('Нет такого имени');
+}
--- README.md
+++ README.md
@@ -3,5 +3,9 @@ https://todorenkod.github.io/
 
 # Homework JS:
 
-* JS 1-2 https://todorenkod.github.io/JS/Homework/01-02/
+### Homework JS 1-2: 
+* JS 1 https://todorenkod.github.io/JS/Homework/01-02/index.html
+* JS 2 https://todorenkod.github.io/JS/Homework/01-02/index2.html
+
+### Homework JS 3-4: 
 * JS 3-4 https://todorenkod.github.io/JS/Homework/03-04/
