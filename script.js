da9999f962c6ef21d4fca8210a10329bd3795e22
Todorenko Dmitriy <todorenko.d@gmail.com>
2017-05-13 11:23:11 +0300

    add files

---
+++ script.js
@@ -0,0 +1,23 @@
+function calcNumbers(x, n) {
+            var result = x;
+
+            for (var i = 1; i < n; i++) {
+                result *= x;
+            }
+
+            return result;
+        }
+
+        var x = prompt("Введите число:", '');
+        var n = prompt("В какую степень возводим?", '');
+
+        if (n < 0) {
+            alert('Степень ' + n + ' не поддерживается, введите число больше 0'
+            );
+        } else if ((n == 0) && (x > 0) || (x < 0)) {
+            		console.log('1');
+        } else if (n == 0 && x == 0) {
+            		alert('0 в степени 0 - не существует');
+        } else{
+            console.log(calcNumbers(x, n));
+        }
