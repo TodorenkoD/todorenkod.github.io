51990aa9ea6052975a5364265b97abcaad26615b
Todorenko Dmitriy <todorenko.d@gmail.com>
2017-06-01 16:09:06 +0300

    add files to hw 9-10

---
+++ JS/Homework/09-10/js/script.js
@@ -0,0 +1,29 @@
+	$(function () {
+            $('.bxslider').bxSlider({
+                auto: true,
+				adaptiveHeight: true,
+            });
+     
+				$('#select').selectmenu();
+			});
+
+	
+	$(function() {
+		$('.dropmenu' ).hover(
+			function(){
+				$(this).children('.sub-menu').slideDown(200);
+			},
+			function(){
+				$(this).children('.sub-menu').slideUp(200);
+			}
+		);
+	});
+	
+	$('input').change(function() {
+	  if ($(this).is(':checked')) {
+		$(this).closest('label').addClass('checked');
+	  } else {
+		$(this).closest('label').removeClass('checked');
+	  }
+	});
+
