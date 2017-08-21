775e9f6afcb013f761389245ccac79e3b98c64e2
Todorenko Dmitriy <todorenko.d@gmail.com>
2017-08-21 11:43:56 +0300

    add Other files

---
+++ Other/course-js/js-1/README.md
@@ -0,0 +1,1 @@
+First version of js-app, just a basic Object usage methods
---
+++ Other/course-js/js-9-es6/src/js/factories.js
@@ -0,0 +1,42 @@
+// class Dog {
+//   constructor(sound) {
+//     this.sound = sound;
+//   }
+//
+//   makeSound() {
+//     console.log(this.sound);
+//   }
+// }
+//
+// const dog =  new Dog('woof');
+//
+// dog.makeSound();
+//
+// const button = document.createElement('button');
+// button.textContent = 'CLICK ME!!!!';
+//
+// document.body.append(button);
+
+// error
+// button.addEventListener('click', dog.makeSound);
+
+// error, this undefined
+// button.addEventListener('click', dog.makeSound.bind(this));
+
+// button.addEventListener('click', () => dog.makeSound());
+
+//
+// function cat (name) {
+//   function makeSound() {
+//     console.log(name);
+//   }
+//
+//   return {
+//     makeSound: makeSound
+//   };
+// }
+//
+// const kitty = cat('meow');
+// kitty.makeSound();
+//
+// button.addEventListener('click', kitty.makeSound);
