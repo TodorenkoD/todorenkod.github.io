a1f66528fc42785976c43023f1cdf73700fb5291
todorenkod <todorenko.d@gmail.com>
2018-07-19 21:51:56 +0200

    add new project Expert

---
+++ HTML/Expert/src/js/scripts.js
@@ -0,0 +1,33 @@
+// Dropdown Menu Fade    
+$(document).ready(function(){
+    $(".nav__item").hover(
+        function() { $('.submenu', this).fadeIn();
+        },
+        function() { $('.submenu', this).fadeOut();
+    });
+});
+
+
+//Modal
+
+
+var modal = document.getElementById('modalMenu');
+
+var btn = document.getElementById("modalMenuBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
